e16b069401f739600f28bd4f30bf162cfac933aa
Alpersalviz <alpersalviz@gmail.com>
2017-02-24 22:51:18 +0300

    popup js düzeltildi

--- web/static/js/popup.js
+++ web/static/js/popup.js
@@ -1,1 +1,1 @@
-window.open("http://127.0.0.1:8000/redirect/popup/"+internet_reklam_url_id+'?userid='+internet_reklam_user_id);
+window.open("http://internetreklam.online/redirect/popup/"+internet_reklam_url_id+'?userid='+internet_reklam_user_id);
